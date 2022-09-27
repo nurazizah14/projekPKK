de67a2c65db5a5aacfc2fdf3b660a7e422a125c1
nurazizah14 <114453827+nurazizah14@users.noreply.github.com>
2022-09-27 10:49:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk PKK</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+        <div class="logo">
+            <h4>Nama web</h4>
+        </div>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="produk.php">Produk</a></li>
+        </ul>
+        <div class="menu-toggle">
+            <input type="checkbox" />
+            <span></span>
+        </div>
+    </nav>
+    <hr>
+    <br>
+    <center>
+    <h2>ANGGOTA KELOMPOK</h2>
+    <table border="1">
+        <tr>
+            <th>Foto</th>
+            <th>biodata</th>
+    </body>
+    
+    <tr>
+        <td><img src="zuy.jpg" width="50"><br> </td>
+        <td>NAMA : NURAZIZAH <br> TTL : TSM,14-09-2005 <br> ALAMAT : PASIRJAYA </td>
+    </tr>
+    <tr>
+        <td><img src="jaem.jpg" width="50"><br> </td>
+        <td>NAMA : SALMA <br> TTL : TSM,15-08-2005 <br> ALAMAT : LENGKONG </td>
+    </tr>
+    </table>
+</html>
---
+++ produk.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk PKK</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+        <div class="logo">
+            <h4>Nama web</h4>
+        </div>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Produk</a></li>
+        </ul>
+        <div class="menu-toggle">
+            <input type="checkbox" />
+            <span></span>
+        </div>
+    </nav>
+    <hr>
+    <br>
+    <h2>PRODUK</h2>
+        <tr>
+        <td><img src="D.jpg" width="150"><br> </td>
+        </tr>
+    <p>>> Dadar gulung</p><br>
+    <p>>> Rp.2.500</p><br>
+    <p>>> Dadar gulung merupakan makanan khas Indonesia dan Malaysia yang dapat digolongkan sebagai panekuk yang diisi dengan parutan kelapa yang dicampur dengan gula jawa cair. Isi ini disebut unti. Kulit dadar gulung identik dengan warna hijau karena diberi pewarna daun suji. </p>
+    <br>
+    <a href="https://instagram.com/zuyyyyyyyyyyy_?igshid=YmMyMTA2M2Y="><button>PESAN</button></a>
+    </body>
+</html>
